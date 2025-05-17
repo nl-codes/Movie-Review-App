b576b1b9fe97a4d0b042495ee2b928df6836a5fe
narayanLohani <narayanlohani07@gmail.com>
2025-05-17 17:29:44 +0545

    Update Path

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
+import { firebaseDetails } from "./details.js";
+
 export const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: firebaseDetails.FIREBASE_API_KEY,
+    authDomain: firebaseDetails.FIREBASE_AUTH_DOMAIN,
+    projectId: firebaseDetails.FIREBASE_PROJECT_ID,
+    storageBucket: firebaseDetails.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: firebaseDetails.FIREBASE_MESSAGING_SENDER_ID,
+    appId: firebaseDetails.FIREBASE_APP_ID,
+    measurementId: firebaseDetails.FIREBASE_MEASUREMENT_ID,
 };
